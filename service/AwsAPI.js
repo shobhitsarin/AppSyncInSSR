888644eb689a843a58604d12a721c20edb49b88d
Rakesh Menon <rmenon3@sapient.com>
2019-05-23 14:21:15 +0530

    fix: SSR issue

--- service/AwsAPI.js
+++ service/AwsAPI.js
@@ -1,23 +1,25 @@
-
 import AWSAppSyncClient from 'aws-appsync';
-import {awsConfig} from './aws-cred-exports';
+import { awsConfig } from './aws-cred-exports';
+import ExecutionEnvironment from 'exenv';
+import fetch from 'node-fetch';
 
-
+global.fetch = fetch;
 
 export default class AwsAppSyncClient {
-    static getClient() {
-        if(AwsAppSyncClient._client) return AwsAppSyncClient._client;
-        return AwsAppSyncClient.createClient();
-    }
-    static createClient() {
+  static getClient() {
+    if (AwsAppSyncClient._client) return AwsAppSyncClient._client;
+    return AwsAppSyncClient.createClient();
+  }
+  static createClient() {
     AwsAppSyncClient._client = new AWSAppSyncClient({
-            url: awsConfig.aws_appsync_graphqlEndpoint,
-            region: awsConfig.aws_appsync_region,
-            auth: {
-              type: awsConfig.aws_appsync_authenticationType,
-              apiKey:awsConfig.aws_appsync_apiKey,
-            },
-          });
-          return AwsAppSyncClient._client;
-    }
+      url: awsConfig.aws_appsync_graphqlEndpoint,
+      region: awsConfig.aws_appsync_region,
+      auth: {
+        type: awsConfig.aws_appsync_authenticationType,
+        apiKey: awsConfig.aws_appsync_apiKey
+      },
+      disableOffline: !ExecutionEnvironment.canUseDOM
+    });
+    return AwsAppSyncClient._client;
+  }
 }
